ff04aac5c80c85033f2a1d8f0bfdbcf9f57106cc
Byron Kruger <bkruger@jhb.dvt.co.za>
2022-09-07 23:20:33 +0200

    Added 3rd type of remote call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,15 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'my-swa';
-  public deezer$?: Observable<any>;
+  public deezerWorking$?: Observable<any>;
+  public deezerNotWorking$?: Observable<any>;
   public fact$?: Observable<any>;
 
   constructor(
     private http: HttpClient
   ) {
-    this.deezer$ = this.http.jsonp('https://api.deezer.com/artist/199' + '?output=jsonp&callback=ng_jsonp_callback_0', 'a')
+    this.deezerWorking$ = this.http.jsonp('https://api.deezer.com/artist/199' + '?output=jsonp&callback=ng_jsonp_callback_0', 'a')
+    this.deezerNotWorking$ = this.http.jsonp('http://api.deezer.com/search/album?q=' + 'eminem' + '&output=jsonp&callback=ng_jsonp_callback_1', 'JSONP_CALLBACK');
     this.fact$ = this.http.get('https://catfact.ninja/fact');
   }
 
